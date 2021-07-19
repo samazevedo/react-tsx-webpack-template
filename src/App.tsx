ab1e673b25e7d35b043c43c2b6378aa2ac138d94
sam-azevedo <samuel.azevedo@live.com>
2021-07-19 17:22:22 -0300

    add webpack config css images svg support

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import './css/app.css'
+import IMAGE from './react-icon.png'
+import SVG from './react.svg'
+
 export const App = () => {
-    return <h1>React Typescript Webpack Starter Template</h1>
+    return <>
+        <h1>React Typescript Webpack Starter Template</h1>
+        <img src={IMAGE} alt="logo" width="300" />
+        <img src={SVG} alt="logo" width="300" />
+
+    </>
 }
