67890cfe486f184dbe28d2679b5a59291c1d8024
sam-azevedo <samuel.azevedo@live.com>
2021-07-22 16:59:33 -0300

    husky test

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import * as React from 'react'
 import IMAGE from './images/react-icon.png'
 import SVG from './images/react.svg'
 import './css/style.css'
-
+const name = "sam";
 const App = () => (
-    const name = "sam";
-<div>
-    <h1>Hello world</h1>
-    <p>Never give up on your dreams!</p>
-</div>
+
+    <div>
+        <h1>Hello world</h1>
+        <p>Never give up on your dreams!</p>
+        <p>the key is consistency</p>
+    </div>
 )
 
 export default App
