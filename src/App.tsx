c0fda0b8de28d61983b5ccfc6e591844b0d83e9d
sam-azevedo <samuel.azevedo@live.com>
2021-07-22 03:42:45 -0300

    add eslint configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import * as React from 'react';
+import * as React from 'react'
 import IMAGE from './images/react-icon.png'
 import SVG from './images/react.svg'
 import './css/style.css'
-const App = () => {
-    return (
-        <>
-            <h1>App is running now</h1>
-            <img src={IMAGE} alt="react-logo" width="300px" />
-            <img src={SVG} alt="react-logo" width="300px" />
-        </>
-    );
-}
 
-export default App;
+
+
+const App = () => (
+    <div>
+        <h1>Hello world</h1>
+        <p>Never give up on your dreams!</p>
+    </div>
+)
+
+export default App
